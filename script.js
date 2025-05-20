519b9f6a5350594bde74bbdb60a532386a0559ef
GuitarClicker <is0608rp@ed.ritsumei.ac.jp>
2025-05-20 21:26:00 +0900

    ログイン機能テスト

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-//FireBaseのログイン機能
-const firebaseConfig = {
-  apiKey: "AIzaSyDMuOygbY8BRPv9nIWCT9Ee6zm1fNbCDVo",
-  authDomain: "guitarclicker-cdc66.firebaseapp.com",
-  projectId: "guitarclicker-cdc66",
-  appId: "1:1073871813234:web:c8e84f7982538e7b2da0eb"
-};
-//Firebaseの初期化
-firebase.initializeApp(firebaseConfig);
-
-// Google認証
-function loginWithGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-
-  firebase.auth().signInWithPopup(provider)
-    .then((result) => {
-      const user = result.user;
-      alert(`ようこそ、${user.displayName} さん！`);
-    })
-    .catch((error) => {
-      console.error(error);
-      alert("ログインに失敗しました。");
-    });
-}
-
 // ボタンを押したときの挙動
 document.getElementById("tapButton").addEventListener("click", () => {
   alert("タップされました！");
